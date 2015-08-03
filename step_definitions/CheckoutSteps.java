02695831ce7faecd628e157967456b94974a36e0
wilson campusano <wilsoncampusanojorge@gmail.com>
2015-08-02 20:44:27 -0400

    java source and compile command

---
+++ step_definitions/CheckoutSteps.java
@@ -0,0 +1,26 @@
+package step_definitions;
+
+import cucumber.api.java.en.*;
+import cucumber.api.*;
+
+public class CheckoutSteps{
+
+	@Given("^the price of a \"(.*?)\" is (\\d+)c$")
+	public void thePriceOfAIsC(String arg1, int arg2) throws Throwable {
+    		// Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^I checkout (\\d+) \"(.*?)\"$")
+	public void iCheckout(int arg1, String arg2) throws Throwable {
+    		// Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the total price should be (\\d+)c$")
+	public void theTotalPriceShouldBeC(int arg1) throws Throwable {
+		    // Write code here that turns the phrase above into concrete actions
+    		throw new PendingException();
+	}
+
+}
